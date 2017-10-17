6034ca712a3a7dbfa68cf70c47cd31adc540e03d
parkcoach <ybm0kr@naver.com>
2017-10-17 17:00:33 +0900

    Poker_success_init

--- poker/src/main/java/poker/Evaluator.java
+++ poker/src/main/java/poker/Evaluator.java
@@ -1,10 +1,12 @@
 package poker;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Evaluator {
-    public String evaluate(Hand hand) {
+
+    public String evaluate_ROYAL_STRAIGHT_FLUSH(Hand hand) {
         if(hand.getCardCount() > 5){
             throw new NotFullHandException();
         }
@@ -12,6 +14,7 @@ public class Evaluator {
         //check royal straight flush
         List<Card> handList = hand.getHandList();
         boolean isRoyalStraightFlush = true;
+
         ArrayList<Integer> list = new ArrayList<Integer>();
         list.add(10); list.add(11); list.add(12); list.add(13); list.add(1);
 
@@ -30,6 +33,84 @@ public class Evaluator {
             return "ROYAL_STRAIGHT_FLUSH";
         }
 
-        return null;
+        return "NOT_MATCH";
     }
+
+    public String evaluate_FOUR_CARD(Hand hand){
+        //check four card
+        if(hand.getCardCount() > 5){
+            throw new NotFullHandException();
+        }
+
+        List<Card> handList = hand.getHandList();
+        int rank_two_count = 0;
+
+        for(Card card : handList){
+            if(card.getRank()==2)
+                rank_two_count++;
+        }
+
+        if(rank_two_count==4){
+            return "FOUR_CARD";
+        }
+
+        return "NOT MATCH";
+    }
+
+    public String evaluate_FLUSH(Hand hand){
+        //check flush
+        if(hand.getCardCount() > 5){
+            throw new NotFullHandException();
+        }
+
+        List<Card> handList = hand.getHandList();
+        Suit suit = handList.get(0).getSuit();
+        boolean isFLUSH = true;
+
+        for(Card card : handList){
+            if(card.getSuit().equals(suit)){
+            }
+            else
+                isFLUSH = false;
+        }
+
+        if(isFLUSH){
+            return "FLUSH";
+        }
+
+        return "NOT MATCH";
+    }
+
+    public String evaluate_STRAIGHT(Hand hand){
+        //check straight
+        if(hand.getCardCount() > 5){
+            throw new NotFullHandException();
+        }
+
+        List<Card> handList = hand.getHandList();
+        ArrayList<Integer> straightList = new ArrayList<Integer>();
+
+        for(Card card : handList){
+            straightList.add(card.getRank());
+        }
+
+        Collections.sort(straightList);
+        int target = straightList.remove(0);
+        boolean isStraight = true;
+
+        for(int x : straightList){
+            if((x - target) == 1){
+                target = x;
+            }
+            else
+                isStraight = false;
+        }
+
+        if(isStraight){
+            return "STRAIGHT";
+        }
+
+        return "NOT MATCH";
+    }
+
 }
--- poker/src/test/java/poker/EvaluatorTest.java
+++ poker/src/test/java/poker/EvaluatorTest.java
@@ -2,12 +2,13 @@ package poker;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class EvaluatorTest {
 
     @Test
-    public void royal_straight_flush(){
+    public void royal_straight_flush_OK(){ //모든 suit가 스페이드이고, rank는 10,11,12,13,1
         Hand hand = new Hand();
         hand.addCard(new Card(10, Suit.SPADE));
         hand.addCard(new Card(11, Suit.SPADE));
@@ -16,8 +17,105 @@ public class EvaluatorTest {
         hand.addCard(new Card(1, Suit.SPADE));
 
         Evaluator eval = new Evaluator();
-        String result = eval.evaluate(hand);
-        System.out.println(result);
+        String result = eval.evaluate_ROYAL_STRAIGHT_FLUSH(hand);
         assertTrue(result.equals("ROYAL_STRAIGHT_FLUSH"));
+    }
+
+    @Test
+    public void royal_straight_flush_ERROR(){ //모든 suit가 스페이드이고, rank는 10,11,12,13,1
+        Hand hand = new Hand();
+        hand.addCard(new Card(10, Suit.SPADE));
+        hand.addCard(new Card(11, Suit.SPADE));
+        hand.addCard(new Card(12, Suit.SPADE));
+        hand.addCard(new Card(13, Suit.HEART));
+        hand.addCard(new Card(1, Suit.SPADE));
+
+        Evaluator eval = new Evaluator();
+        String result = eval.evaluate_ROYAL_STRAIGHT_FLUSH(hand);
+        assertFalse(result.equals("ROYAL_STRAIGHT_FLUSH"));
+    }
+
+    @Test
+    public void four_card_OK(){ //모든 rank는 2이고, suit는 4개 모두
+        Hand hand = new Hand();
+        hand.addCard(new Card(2, Suit.SPADE));
+        hand.addCard(new Card(2, Suit.HEART));
+        hand.addCard(new Card(2, Suit.DIAMOND));
+        hand.addCard(new Card(2, Suit.CLOBA));
+        hand.addCard(new Card(1, Suit.SPADE));
+
+        Evaluator eval = new Evaluator();
+        String result = eval.evaluate_FOUR_CARD(hand);
+        assertTrue(result.equals("FOUR_CARD"));
+    }
+
+    @Test
+    public void four_card_ERROR(){ //모든 rank는 2이고, suit는 4개 모두
+        Hand hand = new Hand();
+        hand.addCard(new Card(2, Suit.SPADE));
+        hand.addCard(new Card(2, Suit.HEART));
+        hand.addCard(new Card(3, Suit.DIAMOND));
+        hand.addCard(new Card(2, Suit.CLOBA));
+        hand.addCard(new Card(1, Suit.SPADE));
+
+        Evaluator eval = new Evaluator();
+        String result = eval.evaluate_FOUR_CARD(hand);
+        assertFalse(result.equals("FOUR_CARD"));
+    }
+
+    @Test
+    public void flush_OK(){ //모든 카드의 suit가 같은 것
+        Hand hand = new Hand();
+        hand.addCard(new Card(1, Suit.DIAMOND));
+        hand.addCard(new Card(3, Suit.DIAMOND));
+        hand.addCard(new Card(4, Suit.DIAMOND));
+        hand.addCard(new Card(7, Suit.DIAMOND));
+        hand.addCard(new Card(1, Suit.DIAMOND));
+
+        Evaluator eval = new Evaluator();
+        String result = eval.evaluate_FLUSH(hand);
+        assertTrue(result.equals("FLUSH"));
+    }
+
+    @Test
+    public void flush_ERROR(){ //모든 카드의 suit가 같은 것
+        Hand hand = new Hand();
+        hand.addCard(new Card(1, Suit.DIAMOND));
+        hand.addCard(new Card(3, Suit.DIAMOND));
+        hand.addCard(new Card(4, Suit.CLOBA));
+        hand.addCard(new Card(7, Suit.DIAMOND));
+        hand.addCard(new Card(1, Suit.DIAMOND));
+
+        Evaluator eval = new Evaluator();
+        String result = eval.evaluate_FLUSH(hand);
+        assertFalse(result.equals("FLUSH"));
+    }
+
+    @Test
+    public void straight_OK(){ //모든 rank가 순서대로 가지고 있는 것
+        Hand hand = new Hand();
+        hand.addCard(new Card(2, Suit.SPADE));
+        hand.addCard(new Card(3, Suit.SPADE));
+        hand.addCard(new Card(4, Suit.SPADE));
+        hand.addCard(new Card(5, Suit.SPADE));
+        hand.addCard(new Card(6, Suit.SPADE));
+
+        Evaluator eval = new Evaluator();
+        String result = eval.evaluate_STRAIGHT(hand);
+        assertTrue(result.equals("STRAIGHT"));
+    }
+
+    @Test
+    public void straight_ERROR(){ //모든 rank가 순서대로 가지고 있는 것
+        Hand hand = new Hand();
+        hand.addCard(new Card(2, Suit.SPADE));
+        hand.addCard(new Card(3, Suit.SPADE));
+        hand.addCard(new Card(4, Suit.SPADE));
+        hand.addCard(new Card(9, Suit.SPADE));
+        hand.addCard(new Card(6, Suit.SPADE));
+
+        Evaluator eval = new Evaluator();
+        String result = eval.evaluate_STRAIGHT(hand);
+        assertFalse(result.equals("STRAIGHT"));
     }
 }
